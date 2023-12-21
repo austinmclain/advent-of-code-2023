fec5b2a8d9a68696111d49eae6fdeb1c4dd95bf9
Austin McLain <93621884+austinmclain@users.noreply.github.com>
2023-12-21 01:53:34 -0600

    Complete Day 1 Part Two

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
 
 /*
  * Open the data file and read each line.
@@ -9,24 +10,66 @@ use std::path::Path;
  * After reading each line, print the sum.
  */
 fn main() {
+    let mut digits = HashMap::new();
+    digits.insert(String::from("one"), 1);
+    digits.insert(String::from("two"), 2);
+    digits.insert(String::from("three"), 3);
+    digits.insert(String::from("four"), 4);
+    digits.insert(String::from("five"), 5);
+    digits.insert(String::from("six"), 6);
+    digits.insert(String::from("seven"), 7);
+    digits.insert(String::from("eight"), 8);
+    digits.insert(String::from("nine"), 9);
+
     let filename = "data/data.txt";
+
     if let Ok(lines) = read_lines(filename) {
         let mut sum = 0;
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(text) = line {
-                for character in text.chars() {
+                // Gets the first integer
+                for i in 0..text.len() {
+                    // Checks if current character is an integer
+                    let character = text.chars().nth(i).unwrap();
                     if character.is_numeric() {
                         let num = character.to_digit(10).unwrap();
                         sum += num * 10;
                         break;
                     }
+                    // Checks if current window contains integer spelled out with letters
+                    let substring = &text[0..i+1];
+                    let found_key = digits.keys().find(|&key| substring.contains(key));
+                    match found_key {
+                        Some(key) => {
+                            let num = digits.get(key).unwrap();
+                            sum += num * 10;
+                            break;
+                        }
+                        None => {
+                        }
+                    }
                 }
-                for character in text.chars().rev() {
+                // Gets the last integer
+                for i in (0..text.len()).rev() {
+                    // Checks if current character is an integer
+                    let character = text.chars().nth(i).unwrap();
                     if character.is_numeric() {
                         let num = character.to_digit(10).unwrap();
                         sum += num;
                         break;
+                    }
+                    // Checks if current window contains integer spelled out with letters
+                    let substring = &text[i..text.len()];
+                    let found_key = digits.keys().find(|&key| substring.contains(key));
+                    match found_key {
+                        Some(key) => {
+                            let num = digits.get(key).unwrap();
+                            sum += num;
+                            break;
+                        }
+                        None => {
+                        }
                     }
                 }
             }
